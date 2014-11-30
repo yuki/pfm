9e4cca55958911bb49ca469d2382421887820b20
Yuki (aka Ruben Gomez) <rugoli@gmail.com>
2014-11-30 21:59:35 +0100

    New helper for tables

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+    def draw_button(type,path)
+        case type
+            when "show"
+                return link_to "", path, :title =>"Show", :class=>"btn btn-success btn-sm glyphicon glyphicon-search"
+            when "edit"
+                return link_to "", path, :title =>"Edit", :class=>"btn btn-warning btn-sm glyphicon glyphicon-edit"
+            when "destroy"
+                return link_to "", path, method: :delete, data: { confirm: 'Are you sure?' }, :title =>"Destroy", :class=>"btn btn-danger btn-sm glyphicon glyphicon-remove"
+
+        end
+    end
 end
